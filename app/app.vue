809dafb66ff411ea5a64aa07cd56499697695859
Homespore165 <Homespore165@gmail.com>
2026-04-05 23:45:03 -0400

    initial commit

---
+++ app/app.vue
@@ -0,0 +1,75 @@
+<script setup>
+useHead({
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+  ],
+  link: [
+    { rel: 'icon', href: '/favicon.ico' }
+  ],
+  htmlAttrs: {
+    lang: 'en'
+  }
+})
+
+const title = 'Unban Circular'
+const description = 'A muti-tool website for duelists'
+
+useSeoMeta({
+  title,
+  description,
+  ogTitle: title,
+  ogDescription: description,
+  ogImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
+  twitterImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
+  twitterCard: 'summary_large_image'
+})
+</script>
+
+<template>
+  <UApp>
+    <UHeader>
+      <template #left>
+        <NuxtLink to="/">
+          <AppLogo class="w-auto h-6 shrink-0" />
+        </NuxtLink>
+      </template>
+
+      <template #right>
+        <UColorModeButton />
+      </template>
+    </UHeader>
+
+    <UMain>
+      <NuxtPage />
+    </UMain>
+
+    <USeparator icon="material-symbols:circle-circle" />
+
+    <UFooter>
+      <template #left>
+        <p class="text-sm text-muted">
+          Built with Nuxt UI • © {{ new Date().getFullYear() }}
+        </p>
+      </template>
+
+      <template #right>
+        <UButton
+          to="https://x.com/Homespore165"
+          target="_blank"
+          icon="i-simple-icons-twitter"
+          aria-label="Twitter"
+          color="neutral"
+          variant="ghost"
+        />
+        <UButton
+          to="https://bsky.app/profile/homespore165.bsky.social"
+          target="_blank"
+          icon="i-simple-icons-bluesky"
+          aria-label="Bluesky"
+          color="neutral"
+          variant="ghost"
+        />
+      </template>
+    </UFooter>
+  </UApp>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <UPageHero
+      title="Unban Circular"
+      description="A multi-tool website for duelists"
+    />
+    <UPageList class="lg:mx-50 mx-25">
+      <UPageCard
+        description="Convert your YDK to JSON importable in official platforms"
+        to="./ydk2json"
+      >
+        <template #title>
+          YDK to JSON
+          <UIcon
+            name="simple-icons:convertio"
+            class="ml-2"
+          />
+        </template>
+      </UPageCard>
+    </UPageList>
+  </div>
+</template>
